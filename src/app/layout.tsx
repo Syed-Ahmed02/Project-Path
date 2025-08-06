37da879061e044bef49feee5ede7cdd8aef6243c
Syed-Ahmed02 <syedahmedd.02@gmail.com>
2025-08-05 20:31:54 -0400

    added bubblegum shadcn theme, added sidebar componenet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import ConvexClientProvider from '@/components/ConvexClientProvider'
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from '@/components/app-sidebar'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,9 +29,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} `}>
+      <SidebarProvider>
+        <AppSidebar />
         <ClerkProvider>
-          <ConvexClientProvider>{children}</ConvexClientProvider>
+          <ConvexClientProvider>
+            <SidebarTrigger />
+            {children}</ConvexClientProvider>
         </ClerkProvider>
+        </SidebarProvider>
       </body>
     </html>
   )
